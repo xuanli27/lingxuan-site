2860efeccedef29031203ad47722013e43759250
qiandu <qiandu@qunhemail.com>
2025-01-10 19:18:47 +0800

    fix: type error

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,26 +1,16 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
   const { messages } = await req.json()
+  console.log(messages)
+  const { textStream } = streamText({
+    model: openai('gpt-4-turbo'),
+    prompt: '你是灵渲科技工作室的AI助手，专门回答有关3D图形、AI和VR技术的问题。',
+  });
 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      { role: 'system', content: '你是灵渲科技工作室的AI助手，专门回答有关3D图形、AI和VR技术的问题。' },
-      ...messages
-    ]
-  })
-
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream)
+  return textStream
 }
 
